989f6c06404f6932d11881b6fb195204d92fac55
Ray Vernagus <r.vernagus@gmail.com>
2022-04-10 00:49:28 +0000

    Extend hello world example

--- hello-world/main.py
+++ hello-world/main.py
@@ -1,5 +1,13 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel, NoneBytes
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool] = None
+
 
 app = FastAPI()
 
@@ -10,3 +18,7 @@ def read_root():
 @app.get('/items/{item_id}')
 def get_item(item_id: int, q: Optional[str]=None):
     return {'item_id': item_id, 'q': q}
+
+@app.put('/items/{item_id}')
+def update_item(item_id: int, item: Item):
+    return {'item_id': item_id, 'item_name': item.name}
